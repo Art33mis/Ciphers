5fde907ba596ad85a418e192514fe173617a1631
Art33mis <artyom12149@gmail.com>
2021-02-15 22:20:29 +0300

    add rec hill

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import affine
 import affine_recurrent
 import simple_replacement
 import hill
+import rec_hill
 
 
 def main():
@@ -11,9 +12,10 @@ def main():
               "1. Шифр простой замены\n"
               "2. Аффинный шифр\n"
               "3. Аффинный рекуррентный шифр\n"
-              "4. Шифр Хилла")
+              "4. Шифр Хилла"
+              "5. Рекуррентный шифр Хилла")
         ch1 = input()
-        if ch1 == "1" or ch1 == "2" or ch1 == "3" or ch1 == "4":
+        if ch1 == "1" or ch1 == "2" or ch1 == "3" or ch1 == "4" or ch1 == "5":
             flag = False
     if ch1 == "1":
         simple_replacement.simple()
@@ -23,6 +25,8 @@ def main():
         affine_recurrent.recurrent()
     if ch1 == "4":
         hill.hill()
+    if ch1 == "5":
+        rec_hill.rec_hill()
 
 
 main()
